179532e358da6ceb5a6802422fd8eff690a815f9
Charrier Simon <simon.charrier42@outlook.fr>
2025-12-04 09:04:42 +0100

    you can logout now

--- Divertissimo/src/app/profile/profile.ts
+++ Divertissimo/src/app/profile/profile.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login/login_service';
+import { Router } from '@angular/router';
+import { Button } from '../components/button/button';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [Button],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {
+export class Profile implements OnInit {
+  username: string = '';
+  password: string = '';
+  loginError: boolean = false;
+  isLoggedIn: boolean = false;
+  constructor(private loginService: LoginService, private router: Router) {}
+  ngOnInit(): void {
+    this.isLoggedIn = this.loginService.isAuthenticated();
+    if (!this.isLoggedIn) {
+      this.router.navigate(['/login']);
+    }
+  }
 
+  Logout() {
+    this.loginService.logout();
+    this.router.navigate(['/login']);
+  }
 }
